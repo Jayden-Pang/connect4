b05c795de3d9840025e0e6f105d954a4ecccfa68
Jayden-Pang <72201738+Jayden-Pang@users.noreply.github.com>
2020-11-21 00:14:47 +0800

    Delete game_manager.h

--- game_manager.h
+++
@@ -1,9 +0,0 @@
-//game_manager.h
-#ifndef GAME_MANAGER_H
-#define GAME_MANAGER_H
-
-void print_guide_screen ();
-void print_selector_screen (int &difficulty, bool &player_start);
-void game_start (int difficulty, bool player_start);
-
-#endif
